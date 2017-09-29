a4a221e84aba44c492fb1abf341b08b0a24b8612
gpapadog <gpapadogeorgou@fas.harvard.edu>
2017-09-29 17:48:17 -0400

    95% CI

--- R/IE_function.R
+++ R/IE_function.R
@@ -14,13 +14,15 @@
 #' @export
 IE <- function(ypop, ypop_var, alpha) {
   
-  ie <- array(NA, dim = c(2, length(alpha), length(alpha)))
-  dimnames(ie) <- list(stat = c('est', 'var'), alpha1 = alpha, alpha2 = alpha)
+  ie <- array(NA, dim = c(4, length(alpha), length(alpha)))
+  dimnames(ie) <- list(stat = c('est', 'var', 'LB', 'UB'), alpha1 = alpha,
+                       alpha2 = alpha)
   
   for (a1 in 1 : length(alpha)) {
     for (a2 in 1 : length(alpha)) {
       ie[1, a1, a2] <- ypop[a1] - ypop[a2]
       ie[2, a1, a2] <- delta_method(ie_var[c(a1, a2), c(a1, a2)])
+      ie[c(3, 4), a1, a2] <- ie[1, a1, a2] + 1.96 * c(- 1, 1) * sqrt(ie[2, a1, a2])
     }
   }
 
