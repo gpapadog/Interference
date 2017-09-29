68e8b2e5c164ec643fa84c4d973f773263b81edf
gpapadog <gpapadogeorgou@fas.harvard.edu>
2017-09-29 17:48:12 -0400

    checking dimensions

--- R/delta_method_function.R
+++ R/delta_method_function.R
@@ -1,4 +1,9 @@
 delta_method <- function(x, vec = c(1, - 1))  {
+  
+  if (length(vec) != dim(x)[1] | length(vec) != dim(x)[2]) {
+    stop('Wrong dimensions.')
+  }
+  
   vec <- matrix(vec, ncol = 1)
   return(t(vec) %*% x %*% vec)
 }
