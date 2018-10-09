5c024e6c540dd99d9e2804c8b77834fe9fef7549
gpapadog <gpapadogeorgou@fas.harvard.edu>
2018-10-09 10:08:30 -0400

    Renaming coefficients argument
    renaming coef_hat to gamma_numer in order to use the same argument name
    for arguments representing the same quantity across different functions

--- R/CalcNumerator_function.R
+++ R/CalcNumerator_function.R
@@ -1,18 +1,19 @@
 #' Calculate the numerator in the estimator Yhat.
 #' 
-#' @param Ai_j A vector of length n_i - 1 including the treatment assignment of all but
-#' the unit we are currently considering.
-#' @param Xi_j The matrix of covariates for all units in the cluster but the one. This
-#' can be the observed values or a counterfactual set of covariates.
-#' @param coef_hat A vector of the ps coefficients starting with the intercept.
+#' @param Ai_j A vector of length n_i - 1 including the treatment assignment of
+#' all but the unit we are currently considering.
+#' @param Xi_j The matrix of covariates for all units in the cluster but the
+#' one. This can be the observed values or a counterfactual set of covariates.
+#' @param gamma_numer A vector of the numerator coefficients starting with the
+#' intercept.
 #' @param alpha The average probability of treatment among the n_i - 1 units.
-#' @param re_alpha The fixed effect bi that gives average propensity of treatment in
-#' the group equal to alpha.
-CalcNumerator <- function(Ai_j, Xi_j, coef_hat, alpha, re_alpha) {
+#' @param re_alpha The fixed effect bi that gives average propensity of
+#' treatment in the group equal to alpha.
+CalcNumerator <- function(Ai_j, Xi_j, gamma_numer, alpha, re_alpha) {
   
-  coef_hat <- matrix(coef_hat, nrow = length(coef_hat), ncol = 1)
+  gamma_numer <- matrix(gamma_numer, nrow = length(gamma_numer), ncol = 1)
   
-  lin_pred <- cbind(1, as.matrix(Xi_j)) %*% coef_hat
+  lin_pred <- cbind(1, as.matrix(Xi_j)) %*% gamma_numer
   lin_pred <- lin_pred + re_alpha
   probs <- expit(lin_pred)
   
